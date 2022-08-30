bec4161f2b820696bac71788d0953d63e42b3db0
Sylar <kmc42692@naver.com>
2022-08-30 11:49:24 +0900

    dispatcher Servlet 기초

---
+++ src/main/java/site/matacoding/ds/UserController.java
@@ -0,0 +1,13 @@
+package site.matacoding.ds;
+
+public class UserController {
+	
+	public void join() {
+		System.out.println("join 호출됨");
+	}
+	
+	public void login() {
+		System.out.println("login 호출됨");
+	}
+	
+}
